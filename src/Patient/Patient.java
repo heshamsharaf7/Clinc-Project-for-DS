785edce160997abe4660f76f942ce61473e8a41c
Ameent3d <ameen.work55@gmail.com>
2021-04-08 12:23:55 +0300

    this is clinic management system

---
+++ src/Patient/Patient.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Patient;
+import Person.Person;
+/**
+ *
+ * @author ameen
+ */
+public class Patient extends Person {
+    
+    public Patient(int id, String name, String address) {
+        super(id, name, address);
+    }
+    
+}
