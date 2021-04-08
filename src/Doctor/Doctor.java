91a737dafff9ac1dabcf51ad12efcb2a0c06e187
Ameent3d <ameen.work55@gmail.com>
2021-04-08 13:21:52 +0300

    this is clinic management system .

--- src/Doctor/Doctor.java
+++ src/Doctor/Doctor.java
@@ -5,15 +5,27 @@
  */
 package Doctor;
 import Person.Person;
+import java.util.LinkedList;
 /**
  *
  * @author ameen
  */
 
 public class Doctor extends Person{
-    
+    LinkedList<Doctor> list= new LinkedList<>();
     public Doctor(int id, String name, String address) {
         super(id, name, address);
     }
+    public void AddDoctor(int id, String name, String address)
+    {
+        
+        Doctor D=new Doctor(id,name,address);
+        list.add(D);
+    }
+    public int NuberOfDoctor()
+    {
+        return list.size();
+    }
+    
     
 }
